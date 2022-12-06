9f4419182acf9403d440ec18b25fb9eb5adc1109
Red Chien <redchien@viridiasolutions.com>
2022-12-06 14:22:04 +0800

    with custom pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  // title = 'pipes';
+  name: string = '';
+  paymentDate: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: unknown;
+
+  car = {
+    make: 'Toyota',
+    model: 'camry',
+    year: 2000,
+  };
+
+  onMilesChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    const parseVal = parseFloat(target.value);
+
+    if (typeof parseVal === 'number') {
+      this.miles = parseVal;
+    }
+    console.log('parseVal:>>', parseVal);
+  }
+
+  onHeightChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.height = parseFloat(target.value);
+  }
+
+  onNameChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.name = target.value;
+  }
+
+  onDateChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.paymentDate = target.value;
+  }
+
+  onAmountChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.amount = parseFloat(target.value);
+  }
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,46 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert',
+})
+export class ConvertPipe implements PipeTransform {
+  transform(value: any, ...args: any[]): any {
+    // console.log('value: >>', value);
+    if (value === 0) return value;
+    if (!value) return '';
+    // if (typeof value === 'number') {
+    console.log('args: >> ', args);
+
+    const unit = args[0];
+    const maxFractionDigits = args[1];
+
+    switch (unit) {
+      case 'km':
+        value = value * 1.60934;
+        break;
+      case 'm':
+        value = value * 1.60934 * 1000;
+        break;
+      case 'cm':
+        value = value * 1.60934 * 1000 * 100;
+        break;
+      default:
+        throw new Error('Target unit not supported');
+    }
+
+    //custom 顯示小數點幾位數內
+    if (typeof maxFractionDigits === 'number') {
+      // console.log(value.toString().split('.'));
+      const valueSplitArr = value.toString().split('.');
+      if (valueSplitArr[1]) {
+        return (
+          valueSplitArr[0] +
+          '.' +
+          valueSplitArr[1].substring(0, maxFractionDigits)
+        );
+      }
+    }
+    return value;
+    // }
+  }
+}
